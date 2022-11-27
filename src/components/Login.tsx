f292081ec1e0033ee7ab0cf675fb48d8fa859f1f
simzub <78310955+simzub@users.noreply.github.com>
2022-11-27 16:10:24 +0200

    initial commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,58 @@
+import { ReactComponent as Svg } from '../spendbook-logo-with-text.svg';
+export default function Login() {
+  return (
+    <>
+      <div className="flex min-h-full items-center justify-center rounded-lg py-12 px-4  sm:px-6 lg:px-8">
+        <div className=" w-full max-w-md  space-y-8 rounded-lg px-8 pt-16 pb-8 shadow-xl">
+          <div className="flex flex-col items-center justify-center">
+            <Svg />
+            <p className="mt-8 text-center text-gray-600 text-base font-inter">Enter you account details to continue</p>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 font-medium text-white text-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3"></span>
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
