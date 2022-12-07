c2d7946e0dcb6c2b43fecedb70137c298c29de9c
Lukas Šivickas <l.sivickas@gmail.com>
2022-12-07 17:19:51 +0200

    Fix login default action

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -23,7 +23,13 @@ export default function Login() {
               Enter you account details to continue
             </p>
           </div>
-          <form className="mt-8 space-y-6" onSubmit={onSubmit}>
+          <form
+            className="mt-8 space-y-6"
+            onSubmit={(e) => {
+              e.preventDefault();
+              onSubmit();
+            }}
+          >
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="flex flex-col gap-8">
               <div>
